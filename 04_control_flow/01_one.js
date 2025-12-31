342cc244b7994af35e724462d3d70084bd4e8898
Muhammad Affaan Qureshi <affaan.qureshi77@gmail.com>
2025-12-31 22:41:27 +0000

    If else conditions and its applications in JS

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,61 @@
+// Control flow is that at one time only some function should be executed not all
+// const isUserLoggedIn = true
+// const temperature = 21
+
+
+// if (temperature < 50) {
+
+//         console.log("Temperature is less than 50");
+        
+
+// } else {
+//     console.log("Temperature is greater than 50");
+// }
+
+// Comparision operator
+// >, <, >=, <=, ==, !=, === it also checks the type, !== it is like the opp of "==="
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    console.log(`User power ${power}`);
+    
+} else {
+
+}
+
+// Short hand notation
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test 2");
+
+// if (balance < 500) {
+
+//     console.log("less than 500")
+// } else if (balance < 750) {
+
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+
+//     console.log("less than 900");
+
+    
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+ const userLoggedIn = true
+ const debitCard = true
+ 
+ if (userLoggedIn && debitCard) {
+    console.log("Allow to buy");
+    
+ }
+ //&& will check both the conditions
+
+
+
