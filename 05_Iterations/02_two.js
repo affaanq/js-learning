68932edfbff9f6c4589918eefad6fdd47614a9f1
Muhammad Affaan Qureshi <affaan.qureshi77@gmail.com>
2026-01-01 23:21:54 +0000

    for of loop and map in JS

--- 05_Iterations/02_two.js
+++ 05_Iterations/02_two.js
@@ -27,3 +27,47 @@ do {
     // afterwards
 
 // It is very common to use loop on Array, there are specialised loops for arrays
+
+//for of loop
+
+const arra = [1, 2, 3, 4, 5]
+// object here means is to whome we have to put the loop
+for (const num of arra) {
+    console.log(num);
+    
+} // No declaration or requirement is required
+
+
+// It can also be used in strings
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    console.log(greet);
+}
+
+// Maps in JS
+
+const map = new Map()
+map.set('UK', 'United Kingdom')
+map.set('UK', 'United Kingdom')
+map.set('Fr', 'France')
+
+console.log(map); // It will print something like object not completely similar
+// it only take unique values no two similar
+
+for (const [key, value] of map) {// detructored array
+    console.log(key, ":-", value); 
+    
+    
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+for (const [key, value] of myObject) {// detructored array
+    console.log(key, ":-", value); 
+
+
+} //My object is not iterable by this method so it will throw an error...
+
