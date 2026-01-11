117c860cd34266fca10c0d4349fe601cbad4fad1
Affaan0604 <affaan.qureshi77@gmail.com>
2026-01-11 21:28:21 +0000

    Refactor game logic and add guess validation for Number Guessing Game

--- projects/project_4/script.js
+++ projects/project_4/script.js
@@ -1,6 +1,6 @@
 // first we have to generate a random values
 
-const random = parseInt(Math.random() * 100 + 1)
+let random = parseInt(Math.random() * 100 + 1)
 
 // now we are taking the values from the html file to the JS to access
 
@@ -25,36 +25,103 @@ let prevGuess = [];
 // it will check are you eligible to play the game
 let playGame = true;
 
+if (playGame) {
+    submit.addEventListener("click", function(e){
+        e.preventDefault();
+
+        const guess = parseInt(userInput.value);
+        validateGuess(guess);
+    })
+}
+
 // it will validate the guess of the user if it is correct or not b/w one to 100
+
+// this is a very important and very common method in the backend
 function validateGuess(guess){
+
+    if (isNaN(guess)) {
+        alert("Please give a valid number")
+    } else if (guess < 1) {
+        alert("Please give a number greater than 1")
+
+    } else if (guess > 100) {
+        alert("Please give a value less than 100")
+
+    } else {
+
+        prevGuess.push(guess) // to put the previous guess value to the array  
+        if (numGuess === 11) {
+            displayGuess(guess)
+            displayMessage(`Game Over, Random number was ${random}`)
+            endGame(); // this will stop the user from further inputting, if the number of guesses are over
+    }   else {
+        displayGuess(guess)
+        checkGuess(guess)
+        
+    }
+
+    }
+
 
 }
 
 // it will check the value and print the result to the web, it will check if the value is or not equal to the random value
 function checkGuess (guess){
 
-
+    if (guess === random) {
+        displayMessage(`You have won the game!`)
+    } else if (guess > random) {
+        displayMessage(`Your guessed number is TOO low!`)
+        
+    }  else if (guess < random) {
+        displayMessage(`Your guessed number is TOO high!`)
+    }
 }
 
 
 // it will interact with DOM, and put the msg if the user wins or loose
 function displayMessage(message){
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
 
 
 }
 
-// will updat the array, guess remaining
+// will update the array, guess remaining... it is a clean up method
 function displayGuess(guess) {
-
-
+    userInput.value = ""; // it cleans the input after each input by the user
+    guessSlot.innerHTML += `${guess}, `;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`
+    
 }
 
 function endGame(){
+    userInput.value = ""
+    userInput.setAttribute("disabled", "");
+    p.classList.add("button");
+    p.innerHTML = `<h2 class= newGame>Start New Game</h2>`
+    startOver.appendChild(p);
+    playGame = false
+    newGame();
 
     
 }
 
 function newGame(){
+    const newGameButton = document.querySelector("#newGame")
+    newGameButton.addEventListener("click", function(e){
+
+        random = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess}`;
+        userInput.removeAttribute("disabled");
+        startOver.removeChild(p);
+        playGame = true;
+
+    })
+
 
 
 }
