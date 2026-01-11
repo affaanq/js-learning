3e1540970013b4080e35c2af58e35b5ea484d65e
Muhammad Affaan Qureshi <affaan.qureshi77@gmail.com>
2026-01-11 18:37:20 +0000

    Refactor event listener for button clicks and improve code readability

--- projects/project_1/script.js
+++ projects/project_1/script.js
@@ -3,23 +3,37 @@ const body = document.querySelector("body")
 
 button.forEach(function (button) {
     console.log(button);
-    button.addEventListener("click", function(e){
-        console.log(e);
-        console.log(e.target); // tels from where the event come from 
+        button.addEventListener("click", function(e){
+                console.log(e);
+                console.log(e.target); // tels from where the event come from 
+                 if(e.target.id === "grey") {
 
-        if(e.target.id === "grey") {
             body.style.backgroundColor = e.target.id
+
         }
+
         else if(e.target.id === "white") {
+
             body.style.backgroundColor = e.target.id
+
         }        
+
         else if(e.target.id === "blue") {
+
             body.style.backgroundColor = e.target.id
+
         }        
+
         else if(e.target.id === "yellow") {
+
             body.style.backgroundColor = e.target.id
+
         }
-        
+
+       
+
     })
+
+
 
 });
