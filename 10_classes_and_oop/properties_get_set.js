0f4b137a1acc26d1e0de624d9c6e4e02c7dc2cca
Affaan0604 <affaan.qureshi77@gmail.com>
2026-01-23 12:04:59 +0000

    Implement User class with getter and setter for email and password

---
+++ 10_classes_and_oop/properties_get_set.js
@@ -0,0 +1,30 @@
+function User (email, password) {
+
+    this.email = email;
+    this.password = password;
+
+    Object.defineProperty(this, "email", {
+
+        get: function(){
+            return this._email.toUpperCase();
+        },
+        set: function(value){
+            this._email = value
+        },
+    })
+
+        Object.defineProperty(this, "password", {
+
+        get: function(){
+            return this._password.toUpperCase();
+        },
+        set: function(value){
+            this._password = value
+        },
+    })
+}
+
+
+const Affaan = new User("affaan@gmai.com", "1233445")
+console.log(Affaan.email)
+
