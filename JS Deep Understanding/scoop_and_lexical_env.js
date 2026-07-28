caf72631fd3efeee27a88fa0075c6be41c070f65
Affaan0604 <affaan.qureshi77@gmail.com>
2026-07-28 13:23:28 +0100

    feat: expand explanation on let, const, and temporal dead zone in JavaScript

--- JS Deep Understanding/scoop_and_lexical_env.js
+++ JS Deep Understanding/scoop_and_lexical_env.js
@@ -19,5 +19,20 @@ a()// JS will first try and find b in the local memory, if not found it will go 
 3. Having the reference of parent's lexical environment means, the child or the local function can access all the variables and functions defined in the memory space of its lexical parent.
 4. The JS engine first searches for a variable in the current local memory space, if its not found here it searches for the variable in the lexical environment of its parent, and if its still not found, then it searches that variable in the subsequent lexical environments, and the sequence goes on until the variable is found in some lexical environment or the lexical environment becomes NULL.
 5. The mechanism of searching variables in the subsequent lexical environments is known as Scope Chain. If a variable is not found anywhere, then we say that the variable is not present in the scope chain.
+*/
+
+// let vs const vs var
+
+// Temporal deadzone: It is the time since "let" variable is hoisted and to which it initialized a value
+
+/*
+1. let and const are hoisted but its memory is allocated at other place than window which cannot be accessed before initialisation.
+2. Temporal Dead Zone exists until variable is declared and assigned a value.
+3. window.variable OR this.variable will not give value of variable defined using let or const.
+4. We cannot redeclare the same variable with let/const(even with using var the second time).
+5. const variable declaration and initialisation must be done on the same line.
+6. There are three types of error: [1] referenceError {given where variable does not have memory allocation} [2] typeError {given when we change type that is not supposed to be changed} [3] syntaxError {when proper syntax(way of writing a statement) is not used}.
+7. Use const wherever possible followed by let, Use var as little as possible(only if you have to). It helps avoid error.
+8. Initialising variables at the top is good idea, helps shrinks TDZ to zero
 
 */
