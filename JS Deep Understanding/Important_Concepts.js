9f64e45a80e6e5f5d822faaddb7edf2004b62a61
Affaan0604 <affaan.qureshi77@gmail.com>
2026-08-03 11:44:17 +0100

    feat: add explanation of closure and its behavior in JavaScript

---
+++ JS Deep Understanding/Important_Concepts.js
@@ -0,0 +1,12 @@
+// WHAT IS THE CLOSURE IN JS?
+/*
+
+It is the function along with its outer enviroment or lexical scope bundled togeather.
+outer()() // double parenthesis is calling the inner function, it is the shortcut for 
+var close = outer();
+close()
+
+-- closure does not change if even we add a parameter to the outer function, it will still print the value.
+
+
+*/
